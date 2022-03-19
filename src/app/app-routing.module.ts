33d45a749f057fa7586810321ba0bbdab449c559
shubhgithubrit <sg4030383@gmail.com>
2022-03-19 12:54:48 -0700

    Error occur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './admin/pages/dashboard/dashboard.component';
 import { LoginComponent } from './admin/pages/login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SlidenavComponent } from './slidenav/slidenav.component';
 
-const approutes: Routes = [
-  { path: 'admin', component: AdminComponent,children:[
+const routes: Routes = [
+  {path: '', redirectTo: 'home',pathMatch:  'full'},
+
     {path:'login',component:LoginComponent},
-    {path:'dashboard',component:DashboardComponent}
-  ]},
+    {path:'dashboard',component:DashboardComponent},
+    {path:'home',component:HomeComponent},
+     
+  
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(approutes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {} 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AdminModule } from './admin/admin.module';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, RouterOutlet, RoutesRecognized } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SlidenavComponent } from './slidenav/slidenav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlidenavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
