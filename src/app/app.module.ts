ae111202ebdb20fb09640e05abc69bf0081c30b9
shubhgithubrit <sg4030383@gmail.com>
2022-04-03 17:53:07 -0700

    Job UI updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, RouterOutlet, RoutesRecognized } from '@angular/router';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +17,12 @@ import { RouterModule, RouterOutlet, RoutesRecognized } from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    FormsModule,
+    MatCardModule,
+    MatToolbarModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
